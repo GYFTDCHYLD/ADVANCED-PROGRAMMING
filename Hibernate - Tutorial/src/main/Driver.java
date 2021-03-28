67d8e0107446df0dfff12b10d0655e5112fbb517
Craig Reid <gyftdchyld@gmail.com>
2021-03-28 15:30:47 -0500

    *

--- Hibernate - Tutorial/src/main/Driver.java
+++ Hibernate - Tutorial/src/main/Driver.java
@@ -6,7 +6,7 @@ import models.Student;
 
 public class Driver {
 	public static void main(String... args) {
-		Student student = new Student(17, "Craig", "Reid", "craig@yahoo.com"); 
+		Student student = new Student(1, "Craig", "Reid", "craig@yahoo.com"); 
 		student.create(); 
 		//student.update();
 		//List list = student.readAll();
