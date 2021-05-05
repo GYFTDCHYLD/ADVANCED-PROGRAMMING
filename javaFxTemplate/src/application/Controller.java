a6914dcc002d656ffcf9449d9aff10ad5f10e424
GYFT'DCHYLD <59238976+GYFTDCHYLD@users.noreply.github.com>
2021-05-05 10:16:11 -0500

    add javaFx template
    template can be used to initialize new java fx project

---
+++ javaFxTemplate/src/application/Controller.java
@@ -0,0 +1,39 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class Controller {
+	@FXML
+    private Label server_status;
+
+    @FXML
+    private Label database_name;
+
+    @FXML
+    private Label host;
+
+    @FXML
+    private Label user;
+
+    @FXML
+    private Label database_status;
+    
+    @FXML
+    private Button server_btn;
+  
+
+    @FXML
+    void startServer(ActionEvent event) {
+    	server_status.setStyle("-fx-background-color: green; -fx-background-radius: 90;");
+    	server_btn.setDisable(true);
+    
+    	database_status.setStyle("-fx-background-color: green; -fx-background-radius: 90;");
+    	
+    	
+    }
+}
